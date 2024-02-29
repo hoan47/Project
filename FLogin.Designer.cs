13b987acc20a7b855593a2676d7404400724090a
VanHung0103 <nvh11103@gmail.com>
2024-02-29 12:18:39 +0700

    Hưng push

--- FLogin.Designer.cs
+++ FLogin.Designer.cs
@@ -30,51 +30,30 @@ namespace Project
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.labelAccount = new System.Windows.Forms.Label();
-            this.textBoxAccount = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
             this.labelPassword = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.labelAccount = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Location = new System.Drawing.Point(12, 3);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(576, 243);
             this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.textBoxAccount);
-            this.panel2.Controls.Add(this.labelAccount);
-            this.panel2.Location = new System.Drawing.Point(4, 9);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(569, 68);
-            this.panel2.TabIndex = 0;
-            // 
-            // labelAccount
-            // 
-            this.labelAccount.AutoSize = true;
-            this.labelAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAccount.Location = new System.Drawing.Point(42, 23);
-            this.labelAccount.Name = "labelAccount";
-            this.labelAccount.Size = new System.Drawing.Size(99, 25);
-            this.labelAccount.TabIndex = 0;
-            this.labelAccount.Text = "Tài khoản";
-            // 
-            // textBoxAccount
-            // 
-            this.textBoxAccount.Location = new System.Drawing.Point(160, 19);
-            this.textBoxAccount.Multiline = true;
-            this.textBoxAccount.Name = "textBoxAccount";
-            this.textBoxAccount.Size = new System.Drawing.Size(344, 29);
-            this.textBoxAccount.TabIndex = 1;
             // 
             // panel3
             // 
@@ -103,6 +82,60 @@ namespace Project
             this.labelPassword.TabIndex = 0;
             this.labelPassword.Text = "Mật khẩu";
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBoxAccount);
+            this.panel2.Controls.Add(this.labelAccount);
+            this.panel2.Location = new System.Drawing.Point(4, 9);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(569, 68);
+            this.panel2.TabIndex = 0;
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Location = new System.Drawing.Point(160, 19);
+            this.textBoxAccount.Multiline = true;
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(344, 29);
+            this.textBoxAccount.TabIndex = 1;
+            // 
+            // labelAccount
+            // 
+            this.labelAccount.AutoSize = true;
+            this.labelAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAccount.Location = new System.Drawing.Point(42, 23);
+            this.labelAccount.Name = "labelAccount";
+            this.labelAccount.Size = new System.Drawing.Size(99, 25);
+            this.labelAccount.TabIndex = 0;
+            this.labelAccount.Text = "Tài khoản";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(7, 157);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(569, 68);
+            this.panel4.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mật khẩu";
+            // 
             // FLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -112,10 +145,12 @@ namespace Project
             this.Name = "FLogin";
             this.Text = "FLogin";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -129,5 +164,8 @@ namespace Project
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.TextBox textBoxAccount;
         private System.Windows.Forms.Label labelAccount;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
