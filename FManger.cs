f51f711d38f3e71fc1ecc5ec8f22033eddcd773c
hoan47 <b2h1604bmt@gmail.com>
2024-02-29 09:47:21 +0700

    Push
    Hoan push

---
+++ FManger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class FManger : Form
+    {
+        public FManger()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
