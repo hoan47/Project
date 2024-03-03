924b80ea48b6f3a99ab09b45c01820069a8ab7c4
hoan47 <b2h1604bmt@gmail.com>
2024-03-03 17:33:15 +0700

    push

--- FForgetPassword.cs
+++ FForgetPassword.cs
@@ -54,6 +54,7 @@ namespace Project
                 ButtonCreateAccountClick(sender, e);
                 e.Handled = true;
             }
+            MessageBox.Show("hi");
         }
 
         private void TextBoxPasswordKeyPress(object sender, KeyPressEventArgs e)
