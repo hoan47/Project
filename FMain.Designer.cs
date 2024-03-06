2ea5047dfcec93a8e785ecc0f4c91e6b4425a366
hoan47 <b2h1604bmt@gmail.com>
2024-03-06 16:43:31 +0700

    push

--- FMain.Designer.cs
+++ FMain.Designer.cs
@@ -51,7 +51,7 @@
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Location = new System.Drawing.Point(200, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1042, 630);
+            this.panel1.Size = new System.Drawing.Size(1040, 630);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
